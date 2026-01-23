0943d3eba369a8d98ccc0126e3446f059194f0e0
woodwoodchin1223-oss <woodwoodchin1223@gmail.com>
2026-01-22 19:35:28 -0800

    lc 443

--- 443.string-compression.cpp
+++ 443.string-compression.cpp
@@ -10,20 +10,20 @@ public:
     int compress(vector<char>& chars) {
         int ans = 0;
         for (int i = 0; i < chars.size();) {
-            char letter = chars[i];
+            char c = chars.at(i);
             int count = 0;
-            while (i < chars.size() && chars[i] == letter) {
+            while (i < chars.size() && chars.at(i) == c) {
                 count += 1;
                 i += 1;
             }
-            chars[ans] = letter;
+            chars[ans] = c;
             ans += 1;
-
-            if (count > 1) {
-                for (char c : to_string(count)) {
-                    chars[ans++] = c;
-                }
+            if (count == 1) continue;
+            for (char cc : to_string(count)) {
+                chars[ans] = cc;
+                ans += 1;
             }
+            
         }
         return ans;
     }
